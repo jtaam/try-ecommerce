8c5fa7bdfed03515f78a3e97b7054d9422f6137e
Tam Jid <tamjid.mdh@gmail.com>
2018-05-27 14:46:09 +0600

    profile page

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
@@ -10,4 +11,12 @@ def home(request):
 def about(request):
     context = {}
     template = 'about.html'
+    return render(request, template, context)
+
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user' : user}
+    template = 'profile.html'
     return render(request, template, context)
