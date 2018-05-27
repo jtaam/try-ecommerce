af1de6fb2494bcc64da84fc435373ac1a5a9ad5e
Tam Jid <tamjid.mdh@gmail.com>
2018-05-27 09:48:51 +0600

    email sent

--- src/contact/views.py
+++ src/contact/views.py
@@ -13,13 +13,7 @@ def contact(request):
         message = '%s %s' %(comment, name)
         emailFrom = form.cleaned_data['email']
         emailTo = [settings.EMAIL_HOST_USER]
-        send_mail(
-            'Subject here',
-            'Here is the message.',
-            'from@example.com',
-            ['to@example.com'],
-            fail_silently=False,
-        )
+        send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
     context = locals()
     template = 'contact.html'
     return render(request, template, context)
